4f8e2cfb71d63545a70072cfb10fb5ba04340bb8
Uchechukwu Akogwu <akogwuuche@ymail.com>
2019-10-13 19:16:24 +0100

    fix: fix heroku error

--- src/components/StoryCard.jsx
+++ src/components/StoryCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import StoryImage from './storyImage'
+import StoryImage from './StoryImage'
 
 const StoryCardComponent = ({ url, title, text }) => {
   return (
